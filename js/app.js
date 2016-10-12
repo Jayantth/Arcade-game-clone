1b9cc2b8660f027c30e65a0be8aa047ff73d9bd8
Jayantth <jayanth.ganesh07@gmail.com>
2016-10-12 17:20:46 +0530

    Object function in global scope

--- js/app.js
+++ js/app.js
@@ -11,6 +11,48 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+ // Check for collision with enemies
+    Enemy.prototype.checkCollision = function() {
+    if (player.p + 131 >= this.y + 90
+        && player.o + 25 <= this.x + 88
+        && player.p + 73 <= this.y + 135
+        && player.o + 76 >= this.x + 11) {
+        console.log('collided');
+        player.o = 200;
+        player.p = 400;
+    }
+
+    // check for player reaching top of canvas and winning the game
+    // if player wins, add 1 to the score and level
+    // pass score as an argument to the increaseDifficulty function
+    if (player.p + 63 <= 0) {
+        player.o = 202.5;
+        player.p = 383;
+        console.log('you made it!');
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+
+        score += 1;
+        gameLevel += 1;
+        console.log('current score: ' + score + ', current level: ' + gameLevel);
+        increaseDifficulty(score);
+
+    }
+
+    // check if player runs into left, bottom, or right canvas walls
+    // prevent player from moving beyond canvas wall boundaries
+    if (player.p > 383 ) {
+        player.p = 383;
+    }
+    if (player.o > 402.5) {
+        player.o = 402.5;
+    }
+    if (player.o < 2.5) {
+        player.o = 2.5;
+    }
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -23,9 +65,8 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= 505) {
         this.x = 0;
     }
-
-    // Check for collision with enemies or barrier-walls
-    checkCollision(this);
+    // Check for collision with enemies
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,9 +77,9 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (x, y, speed) {
-    this.x = x;
-    this.y = y;
+var Player = function (o, p, speed) {
+    this.o = o;
+    this.p = p;
     this.speed = speed;
     this.sprite = 'images/char-horn-girl.png';
 };
@@ -49,22 +90,22 @@ Player.prototype.update = function(dt) {
 // Draw the player on the screen, required method for game
 // Display score
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.o, this.p);
     displayScoreLevel(score, gameLevel);
 };
 
 Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'left') {
-        player.x -= player.speed;
+        this.o -= this.speed;
     }
     if (keyPress == 'up') {
-        player.y -= player.speed;
+        this.p -= this.speed;
     }
     if (keyPress == 'right') {
-        player.x += player.speed;
+        this.o += this.speed;
     }
     if (keyPress == 'down') {
-        player.y += player.speed;
+        this.p += this.speed;
     }
     console.log('keyPress is: ' + keyPress);
 };
@@ -80,48 +121,6 @@ var displayScoreLevel = function(Scores, Levels) {
     document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
 };
 
-var checkCollision = function(anEnemy) {
-    // check for collision between enemy and player
-    if (
-        player.y + 131 >= anEnemy.y + 90
-        && player.x + 25 <= anEnemy.x + 88
-        && player.y + 73 <= anEnemy.y + 135
-        && player.x + 76 >= anEnemy.x + 11) {
-        console.log('collided');
-        player.x = 50;
-        player.y = 305;
-    }
-
-    // check for player reaching top of canvas and winning the game
-    // if player wins, add 1 to the score and level
-    // pass score as an argument to the increaseDifficulty function
-    if (player.y + 63 <= 0) {
-        player.x = 202.5;
-        player.y = 383;
-        console.log('you made it!');
-
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 505, 171);
-
-        score += 1;
-        gameLevel += 1;
-        console.log('current score: ' + score + ', current level: ' + gameLevel);
-        increaseDifficulty(score);
-
-    }
-
-    // check if player runs into left, bottom, or right canvas walls
-    // prevent player from moving beyond canvas wall boundaries
-    if (player.y > 383 ) {
-        player.y = 383;
-    }
-    if (player.x > 402.5) {
-        player.x = 402.5;
-    }
-    if (player.x < 2.5) {
-        player.x = 2.5;
-    }
-};
 
 // Increase number of enemies on screen based on player's score
 var increaseDifficulty = function(numEnemies) {
@@ -142,7 +141,7 @@ var increaseDifficulty = function(numEnemies) {
 // Enemy randomly placed vertically within section of canvas
 // Declare new score and gameLevel variables to store score and level
 var allEnemies = [];
-var player = new Player(202.5, 383, 50);
+var player = new Player(202.5, 383, 40);
 var score = 0;
 var gameLevel = 1;
 var scoreLevelDiv = document.createElement('div');
